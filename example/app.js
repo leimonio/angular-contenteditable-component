2a129a419abfb42ad5ff2bb4510951eee1126b36
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-03-24 21:06:25 +0200

    Add simple example for library

---
+++ example/app.js
@@ -0,0 +1,25 @@
+import angular from 'angular';
+import ContentEditable from '../src/angular-contenteditable-component';
+
+class AppController {
+  constructor(){}
+  $onInit(){
+    this.text = 'This is a random text';
+  }
+  toggleFocus() {
+    this.active = !this.active;
+  }
+  updateText(text) {
+    console.log('Text updated:', text);
+    this.text = text;
+  }
+  editApply(text){
+    console.log('Text applied:', text);
+    this.text = text;
+    this.active = false;
+  }
+}
+
+angular.module('app', [ContentEditable]);
+angular.module('app')
+    .controller('AppController', AppController);
