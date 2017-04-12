1d0e30d8528414f802326c1e046723b9d727acf1
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-04-12 10:07:08 +0300

    Add karma configuration files for files

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.spec.js'
+      './node_modules/angular/angular.js',
+      './node_modules/angular-mocks/angular-mocks.js',
+      './src/angular-contenteditable-component.js',
+      './src/angular-contenteditable-component.spec.js'
     ],
 
 
