8a05dc1f70ce44dd2f7c1f985e9829d552255bdf
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-04-16 23:52:06 +0300

    Update tests for component with no bindings

--- src/angular-contenteditable-component.spec.js
+++ src/angular-contenteditable-component.spec.js
@@ -2,31 +2,40 @@ describe('Component:ContentEditable', function() {
 
   beforeEach(angular.mock.module('ContentEditable'));
 
-  let scope, controller;
-  beforeEach(inject(($rootScope, $compile) => {
-    scope = $rootScope.$new();
-    element = angular.element('<content-editable>Some random text here</content-editable>');
-    element = $compile(element)(scope);
-    scope.$apply();
-  }));
+  describe('without bindings', () => {
 
-  let vm;
-  let actual, expected;
-  describe('initialization', () => {
-    beforeEach(() => {
-        vm = element.controller('contentEditable');
-    });
+    let scope, element;
+    beforeEach(inject(($rootScope, $compile) => {
+      scope = $rootScope.$new();
+      element = angular.element('<content-editable>Some random text here</content-editable>');
+      element = $compile(element)(scope);
+      scope.$apply();
+    }));
 
-    it('constructor', () => {
-      expect(vm.$element).toBeDefined();
-      expect(vm.$timeout).toBeDefined();
-    });
+    let $ctrl;
+    let actual, expected;
+    describe('initialization', () => {
+      beforeEach(() => {
+        $ctrl = element.controller('contentEditable');
+      });
 
-    it('should set the bindings', () => {
-      expect(vm.active).toBeUndefined();
-      expect(vm.onApply).toBeUndefined();
-      expect(vm.onChange).toBeUndefined();
-      expect(vm.maxLength).toBeUndefined();
+      it('constructor', () => {
+        expect($ctrl.$element).toBeDefined();
+        expect($ctrl.$timeout).toBeDefined();
+      });
+
+      it('should set the bindings', () => {
+        expect($ctrl.active).toBeUndefined();
+        expect($ctrl.onApply).toBeUndefined();
+        expect($ctrl.onChange).toBeUndefined();
+        expect($ctrl.maxLength).toBeUndefined();
+      });
+
+      it('should initialize values', () => {
+        expect($ctrl.isEditable).toBeDefined();
+        expect($ctrl.isEditable).toBe(true);
+        expect($ctrl.$elem).toBeDefined();
+      })
     });
   });
 });
