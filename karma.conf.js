80b574ee94e14e83f93768ae33cf07d106691078
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-04-21 00:32:52 +0300

    Add configuration for TravisCI to support chrome browser for tests

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
 
 
     // Continuous Integration mode
@@ -68,6 +68,13 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
   })
 }
